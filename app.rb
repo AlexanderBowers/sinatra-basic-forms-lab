f1aacaa69cde73af6eb0ab0b36e0d5776e8f1f60
AlexanderBowers <7319abowers@gmail.com>
2020-12-10 19:46:10 -0800

    finished lab

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :index
+    end
+
+    get '/puppy' do
+        erb :display_puppy
+    end
+
+    get '/new' do
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        @puppy = params
+        erb :display_puppy
+    end
 
 end
